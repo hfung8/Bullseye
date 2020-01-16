2622222682df892145bbd6e97b276ba05b77c911
Harrison Fung <harrison.hok.fung@gmail.com>
2020-01-16 10:20:46 -0500

    Initial Commit

---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Harrison Fung  on 1/16/20.
+//  Copyright © 2020 Harrison Fung . All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
